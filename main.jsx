bdbd0a7a7c97c664d036e269fa0e94293522ac69
Yeimy Alejandra Sánchez Gil <100418188+yeimy07@users.noreply.github.com>
2026-03-05 13:49:22 -0500

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PortfolioAnalyzer from './portfolio-analyzer.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PortfolioAnalyzer />
+  </React.StrictMode>,
+)
